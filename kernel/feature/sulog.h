f043eb751a217aee2871a84cbe23f984e5859e92
mistrmochov <matejdvoracek@fastmail.com>
2026-07-01 04:04:27 +0200

    feat(sulog): Fix compiling sulog header on kernel > 6.1 & x86_64

--- kernel/feature/sulog.h
+++ kernel/feature/sulog.h
@@ -3,6 +3,10 @@
 
 #include <linux/types.h>
 #include <linux/init.h>
+#include <linux/version.h>
+#if defined(__x86_64__) && LINUX_VERSION_CODE > KERNEL_VERSION(6, 1, 0)
+#include <linux/mm.h>
+#endif
 
 bool ksu_sulog_is_enabled(void);
 void __init ksu_sulog_init(void);
